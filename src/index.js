81ff877ba90b997d88406f0b0daee5876cab98e3
1ackbfun <lackbfun@protonmail.com>
2022-06-10 00:35:13 +0800

    ci: really last try

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello era! @6.10', {
+  return new Response('Hello era #1! @6.10', {
     headers: { 'content-type': 'text/plain' },
   })
 }
